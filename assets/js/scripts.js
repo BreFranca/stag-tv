874ca485b4a7e28cb4e101674f15504d919bb42d
Breno Franca <breno.franca@lipperhub.com>
2018-06-29 08:55:23 -0300

    Insert counter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,30 @@ $('.slider-words').slick({
     arrows: false,
     dots: false
 })
+
+function getSecs(sHors, sMins, sSecs, campo) {
+    sSecs++;
+
+    if (sSecs === 60) {
+        sSecs = 0;
+        sMins++;
+    }
+    if (sMins === 60) {
+        sMins = 0;
+        sHors++;
+    }
+    if (sSecs <= 9) {
+        sSecs = "0" + sSecs;
+    }
+    if (sMins <= 9) {
+        sMins = "0" + sMins;
+    }
+    if (sHors <= 9) {
+        sHors = "0" + sHors;
+    }
+    document.getElementById(campo).innerHTML = sHors + ":" + sMins + ":" + sSecs;
+
+    setTimeout("getSecs(" + sHors + ", " + sMins + "," + sSecs + ", '" + campo + "')", 1000);
+}
+
+getSecs(0, 0, 0, 'timer')
